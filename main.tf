35e0bd2c84f30365032742c8846a3e43868d82de
satandyh <8116964+satandyh@users.noreply.github.com>
2020-02-10 21:10:12 +0300

    use centos8

--- main.tf
+++ main.tf
@@ -18,7 +18,8 @@ resource "random_pet" "zoo" {
 
 # create volumes
 resource "libvirt_volume" "rh" {
-  source = "/var/lib/libvirt/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  #source = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
+  source = "/var/lib/libvirt/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
   format = "qcow2"
   name   = "${element(random_pet.zoo.*.id, count.index)}.qcow2"
   pool   = "default"
